f5790d8a8c706277c9b672cc2843092bdffbafe3
Greg Edelston <gredelston@google.com>
2020-02-20 10:36:07 -0700

    Run the quiz in a class, so that we can track state easier

--- quizme.py
+++ quizme.py
@@ -15,6 +15,7 @@ try:
 except NameError:
     pass
 
+
 class QuizEntry(object):
     """Data from a single row of the source CSV."""
     def __init__(self, prompt, answer, categories=None, other_answers=None):
@@ -52,6 +53,110 @@ class QuizEntry(object):
         return s
 
 
+class QuizGame(object):
+    """Class to run the actual quiz-game."""
+    def __init__(self, args):
+        """
+        @param args: hashable object of command-line args
+
+        """
+        self.args = args
+        self.quiz = args.quiz
+        self.load_quiz_entries()
+        self.filter_entries_by_category(args.category)
+        if args.show_categories:
+            self.display_categories()
+            sys.exit(0)
+        if args.show_data:
+            self.display_entries()
+            sys.exit(0)
+
+    def load_quiz_entries(self):
+        """Load the quiz CSV into a list of QuizEntry objects."""
+        quizme_dir = os.path.dirname(os.path.abspath(__file__))
+        csv_path = os.path.join(quizme_dir, '%s.csv' % self.quiz)
+        with open(csv_path)as csvfile:
+            quizreader = csv.DictReader(csvfile)
+            self.entries = []
+            for row in quizreader:
+                prompt = row['prompt']
+                answer = row['answer']
+                categories = row.get('categories', None)
+                other_answers = row.get('other_answers', None)
+                if not prompt or not answer:
+                    print('Invalid CSV row (must contain prompt and answer):')
+                    print(row)
+                    sys.exit(1)
+                entry = QuizEntry(prompt, answer, categories, other_answers)
+                self.entries.append(entry)
+
+    def filter_entries_by_category(self, cat):
+        """Return a list of entries that contain the given category."""
+        if not cat:
+            return
+        all_categories = self.get_categories()
+        if cat not in all_categories:
+            print('Invalid category: %s' % cat)
+            print('Categories for this quiz: %s' % ', '.join(all_categories))
+            sys.exit(1)
+        self.entries = list(filter(lambda e: cat in e.categories, self.entries))
+
+    def get_categories(self):
+        """Return a list of categories in this quiz."""
+        categories = []
+        for entry in self.entries:
+            for category in entry.categories:
+                if category not in categories:
+                    categories.append(category)
+        return categories
+
+    def display_categories(self):
+        """Display all categories in this quiz."""
+        categories = self.get_categories()
+        print("Valid categories for quiz '%s':" % self.quiz)
+        print(', '.join(categories))
+
+    def display_entries(self):
+        """Display all the entries in this quiz."""
+        for e in self.entries:
+            print(e)
+
+    def run_quiz(self):
+        """Main quiz-running function."""
+        print('Quizzing on %s%s' % (self.quiz,
+                (' (%s)' % self.args.category) if self.args.category else ''))
+        print('Respond with %s anytime to quit.' % '/'.join(EXIT_STRINGS))
+        print('')
+        self.questions_asked = 0
+        self.score = 0
+        while True:
+            e = random.choice(self.entries)
+            print('%d/%d # %s' % (self.score, self.questions_asked, e.prompt))
+            response = input('> ').strip()
+            if response.lower() in EXIT_STRINGS:
+                return
+            elif e.check_answer(response):
+                print('Correct! 😊')
+                self.score += 1
+            else:
+                print('Incorrect. Correct answer was: %s' % e.answer)
+                for other_answer in e.other_answers:
+                    print('We also would have accepted: %s' % other_answer)
+                incorrect_entry = self.get_entry_by_answer(response)
+                if incorrect_entry:
+                    print('You gave the right answer for %s.' %
+                            incorrect_entry.prompt)
+            self.questions_asked += 1
+            print('')
+
+    def get_entry_by_answer(self, answer):
+        """Find the quiz entry whose answer is `answer` (else None)."""
+        for entry in self.entries:
+            if entry.check_answer(answer):
+                return entry
+        return None
+
+
 def all_quizzes():
     """Return a list of available quizzes."""
     quizme_dir = os.path.dirname(os.path.abspath(__file__))
@@ -77,111 +182,11 @@ def parse_args():
     return parser.parse_args(sys.argv[1:])
 
 
-def display_categories(args, quiz_entries):
-    """Given a list of quiz_entries, display all categories."""
-    categories = get_categories(quiz_entries)
-    print("Valid categories for quiz '%s':" % args.quiz)
-    print(', '.join(categories))
-
-
-def display_entries(quiz_entries):
-    """Given a list of quiz_entries, display them all."""
-    for e in quiz_entries:
-        print(e)
-
-
-def load_quiz_entries(quiz):
-    """Load the quiz CSV into a list of QuizEntry objects."""
-    quizme_dir = os.path.dirname(os.path.abspath(__file__))
-    csv_path = os.path.join(quizme_dir, '%s.csv' % quiz)
-    with open(csv_path)as csvfile:
-        quizreader = csv.DictReader(csvfile)
-        entries = []
-        for row in quizreader:
-            prompt = row['prompt']
-            answer = row['answer']
-            categories = row.get('categories', None)
-            other_answers = row.get('other_answers', None)
-            if not prompt or not answer:
-                print('Invalid CSV row (must contain prompt and answer):')
-                print(row)
-                sys.exit(1)
-            entry = QuizEntry(prompt, answer, categories, other_answers)
-            entries.append(entry)
-    return entries
-
-
-def filter_entries_by_category(entries, cat):
-    """Return a list of entries that contain the given category."""
-    if not cat:
-        return entries
-    all_categories = get_categories(entries)
-    if cat not in all_categories:
-        print('Invalid category: %s' % cat)
-        print('Categories for this quiz: %s' % ', '.join(all_categories))
-        sys.exit(1)
-    return list(filter(lambda entry: cat in entry.categories, entries))
-
-
-def get_entry_by_answer(entries, answer):
-    """Find the quiz entry whose answer is `answer` (else None)."""
-    for entry in entries:
-        if entry.check_answer(answer):
-            return entry
-    return None
-
-
-def get_categories(quiz_entries):
-    """From a list of quiz entries, return a list of unique categories."""
-    categories = []
-    for entry in quiz_entries:
-        for category in entry.categories:
-            if category not in categories:
-                categories.append(category)
-    return categories
-
-
-def run_quiz(args, quiz_entries):
-    """Main quiz-running function."""
-    print('Quizzing on %s%s' % (args.quiz,
-            (' (%s)' % args.category) if args.category else ''))
-    print('Respond with %s anytime to quit.' % '/'.join(EXIT_STRINGS))
-    print('')
-    questions_asked = 0
-    score = 0
-    while True:
-        e = random.choice(quiz_entries)
-        print('%d/%d # %s' % (score, questions_asked, e.prompt))
-        response = input('> ').strip()
-        if response.lower() in EXIT_STRINGS:
-            sys.exit(0)
-        elif e.check_answer(response):
-            print('Correct! 😊')
-            score += 1
-        else:
-            print('Incorrect. Correct answer was: %s' % e.answer)
-            for other_answer in e.other_answers:
-                print('We also would have accepted: %s' % other_answer)
-            incorrect_entry = get_entry_by_answer(quiz_entries, response)
-            if incorrect_entry:
-                print('You gave the right answer for %s.' %
-                        incorrect_entry.prompt)
-        questions_asked += 1
-        print('')
-
-
 def main():
     """Main program function."""
     args = parse_args()
-    quiz_entries = load_quiz_entries(args.quiz)
-    if args.show_categories:
-        display_categories(args, quiz_entries)
-        sys.exit(0)
-    quiz_entries = filter_entries_by_category(quiz_entries, args.category)
-    if args.show_data:
-        display_entries(quiz_entries)
-        sys.exit(0)
-    run_quiz(args, quiz_entries)
+    quiz_game = QuizGame(args)
+    quiz_game.run_quiz()
 
 if __name__ == '__main__':
     main()
