e335e09ae03e0ca59433aec5df98f75055244f7f
Greg Edelston <gredelston@google.com>
2020-02-25 10:38:16 -0700

    Add some naive answer-checking feedback

--- gui.py
+++ gui.py
@@ -7,7 +7,10 @@ class GameUI:
         self.master = master
         master.title('QuizMe!')
 
-        self.prompt_label = tk.Label(master, text='Prompt will go here.')
+        self.top_label = tk.Label(master, text='Sample quiz, yo.')
+        self.top_label.pack()
+
+        self.prompt_label = tk.Label(master, text='> Capital of Botswana')
         self.prompt_label.pack()
 
         self.answer_entry = tk.Entry(master)
@@ -18,11 +21,20 @@ class GameUI:
                 command=self.submit_answer)
         self.answer_button.pack()
 
+        self.feedback_text = tk.StringVar()
+        self.feedback_label = tk.Label(master, textvariable=self.feedback_text)
+        self.feedback_label.pack()
+
         self.close_button = tk.Button(master, text='Close', command=master.quit)
         self.close_button.pack()
 
     def submit_answer(self, event=None):
-        print('Answer: %s' % self.answer_entry.get())
+        answer = self.answer_entry.get()
+        print('Submitted answer: %s' % answer)
+        if answer.lower() == 'gaborone':
+            self.feedback_text.set('Correct!')
+        else:
+            self.feedback_text.set('Incorrect. The correct answer was: Gaborone')
 
     def greet(self):
         print('Greetings!')
